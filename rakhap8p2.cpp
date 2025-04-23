bd7196b9eea9756a2a3e801b8ac0b4e592953a37
Linuxerr8 <rakasuharto119@gmail.com>
2025-04-23 18:58:21 +0700

    Add files via upload
    CODE DEV C++ FOR OUTPUT SIMPLE IF "HALO"

---
+++ rakhap8p2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string kata;
+    cout << "Masukkan kata = HALO" << endl;
+    cin >> kata;
+
+    if (kata == "HALO") {
+        cout << "Kata yang dimasukkan sesuai" << endl;
+    } else {
+        cout << "Kata yang dimasukkan tidak sesuai" << endl;
+    }
+
+    return 0;
+}
